a39f1d065c013d7c39e91a9b40fdd6ace885d361
Rohan Nayan <rohannayan405@gmail.com>
2023-06-14 00:21:54 +0530

    Added Feature Component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import './feature.css';
 
-function Feature(){
+function Feature(props){
     return(
-        <div>
-            Feature
+        <div className="gpt4_features-container_feature">
+            <div className="gpt4_features-container_feature-title">
+                <div />
+                <h1>{props.title}</h1>
+            </div>
+            <div className="gpt4_features-container_feature-text">
+                <p>{props.text}</p>
+            </div>
         </div>
     );
 }
