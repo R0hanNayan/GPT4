225e495a8705d89f17e2c2bfa2f2133293584244
Rohan Nayan <rohannayan405@gmail.com>
2023-06-13 23:44:05 +0530

    Added WhatGPT Component

--- src/containers/whatGPT4/WhatGPT4.jsx
+++ src/containers/whatGPT4/WhatGPT4.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import './whatGPT4.css';
+import { Feature } from "../../components";
 
 function WhatGPT4(){
     return(
-        <div>
-            WhatGPT4
+        <div className="gpt4_whatgpt4 section_margin" id="wgpt4">
+            <div className="gpt4_whatgpt4-feature">
+                <Feature/>
+            </div>
+            <div className="gpt4_whatgpt4-heading">
+                <h1 className="gradient_text">The possibilities are beyond your imagination</h1>
+                <p>Explore The Library</p>
+            </div>
+            <div className="gpt4_whatgpt4-container">
+                <Feature/>
+                <Feature/>
+                <Feature/>
+            </div>
         </div>
     );
 }
