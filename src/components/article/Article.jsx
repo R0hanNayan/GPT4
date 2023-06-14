c2aae0420d1ca353b2de8636139e1695df03c7c0
Rohan Nayan <rohannayan405@gmail.com>
2023-06-14 23:23:29 +0530

    Added Article Component

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import './article.css';
 
-function Article(){
+function Article(props){
     return(
-        <div>
-            Article
+        <div className="gpt4_blog-container_article">
+            <div className="gpt4_blog-container_article-image">
+                <img src={props.imgUrl} alt="blog"/>
+            </div>
+            <div className="gpt4_blog-container_article-content">
+                <div>
+                    <p>{props.date}</p>
+                    <h3>{props.title}</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
         </div>
     );
 }
